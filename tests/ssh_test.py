7836e43a4b22d09bcf66928fd46711b3b483b1c5
Kirby <knoverman@comcast.net>
2019-03-13 10:46:35 -0600

    added ssh connection exceptions

--- tests/ssh_test.py
+++ tests/ssh_test.py
@@ -13,9 +13,19 @@ user = "pi"
 passw = "pi"
 
 # Establish SSH tunnel
-ssh = paramiko.SSHClient()
-ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh.connect(hostname=ip, username=user, password=passw)
+try:
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(hostname=ip, username=user, password=passw)
+except BadHostKeyException:
+    print("Host could not be found.")
+    sys.exit(0)
+except AuthenticationException:
+    print("Could not authenticate host.")
+    sys.exit(0)
+except SSHException:
+    print("Unknown SSH error.")
+    sys.exit(0)
 
 # Give stdin to this script
 tty.setcbreak(sys.stdin)
@@ -36,4 +46,6 @@ finally:
     old = termios.tcgetattr(fd)
     old[3] = old[3] | termios.ECHO
     termios.tcsetattr(fd, termios.TCSADRAIN, old)
+    # Close SSH tunnel
+    ssh.close()
 
